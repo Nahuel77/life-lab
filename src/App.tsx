5875c0d88ba28323691512701c69ae0e20407554
Nahuel <nahuelastor@gmail.com>
2024-12-22 10:00:30 -0300

    reglas

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='contenedor'>
         <h1>Life Lab</h1>
         <Tablero></Tablero>
+        <div>
+          <p>1. Cualquier célula viva con dos o tres vecinos vivos sobrevive.</p>
+          <p>2. Cualquier célula muerta con tres vecinos vivos se convierte en una célula viva.</p>
+          <p>3. Todas las demás células vivas mueren en la próxima generación. Del mismo modo, todas las demás células muertas permanecen muertas.</p>
+        </div>
       </div>
     </>
   )
