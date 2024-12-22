3701f7041dfd6e7cb158d24c127b3bf21628cffe
Nahuel <nahuelastor@gmail.com>
2024-12-22 06:57:37 -0300

    primer commit remoto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+    <h1>Hola Mundo. Vayanse a la mierda</h1>
+    </>
+  )
+}
+
+export default App
